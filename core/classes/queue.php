2bd2612fe09db82a3df51913b55459a1f0a5c6e0
Valentin Balt <valentin.balt@gmail.com>
2014-10-23 20:11:50 +0300

    Parent relation for queue

--- core/classes/queue.php
+++ core/classes/queue.php
@@ -108,7 +108,7 @@ class Queue extends Base
 			$id = intval($id);
 			$query = "select * from queue where id=$id";
 		} else {
-			$query = "select * from queue where processed=0 and skipped=0 and brand=:brand order by created limit 1";
+			$query = "select * from queue where processed=0 and skipped=0 and brand=:brand order by id limit 1";
 		}
 		
 		$st = $this->db()->prepare($query);
@@ -126,13 +126,17 @@ class Queue extends Base
 	{
 		$id = intval($id);
 		$manager = LOGGED;
+
 		$query = "update queue set processed=1 where id=$id";
-		
-		
 		if (!$this->db()->query($query)) {
 			return false;
 		}
-		
+
+		$query = "update queue set processed=0 where parent_id=$id and processed=2 and skipped=0";
+		if (!$this->db()->query($query)) {
+			return false;
+		}
+
 		return true;
 	}
 	
@@ -149,9 +153,9 @@ class Queue extends Base
 		return true;
 	}
 	
-	public function create($task, $params, $owner=null, $owner_id=null)
+	public function create($task, $params, $owner=null, $owner_id=null, $parent_id=null)
 	{
-		$query = "insert into queue (task, params, brand, owner, owner_id) values (:task, :params, :brand, :owner, :owner_id)";
+		$query = "insert into queue (task, params, brand, owner, owner_id, parent_id, processed) values (:task, :params, :brand, :owner, :owner_id, :parent_id, :processed)";
 		
 		$st = $this->db()->prepare($query);
 		
@@ -168,11 +172,19 @@ class Queue extends Base
 		} else {
 			$st->bindValue(':owner_id', null, PDO::PARAM_NULL);
 		}
-		
+		if ($parent_id) {
+			$st->bindValue(':parent_id', $parent_id, PDO::PARAM_INT);
+			// Keep for next processing
+			$st->bindValue(':processed', 2, PDO::PARAM_INT);
+		} else {
+			$st->bindValue(':parent_id', null, PDO::PARAM_NULL);
+			$st->bindValue(':processed', 0, PDO::PARAM_INT);
+		}
+
 		return $st->execute() ? true : false;
 	}
 	
-	public function getList($task, $processed=false, $owner=null, $owner_id=null, $sorting=null)
+	public function getList($task, $processed=false, $owner=null, $owner_id=null, $sorting=null, $parent_id=null)
 	{
 		$processed = $processed ? 1 : 0;
 		$sort = ($sorting==self::SORT_DESC) ? 'desc' : 'asc';
@@ -183,6 +195,9 @@ class Queue extends Base
 		}
 		if ($owner_id) {
 			$query .= ' and owner_id=:owner_id';
+		}
+		if ($parent_id) {
+			$query .= ' and parent_id=:parent_id';
 		}
 		$query .= " order by id $sort limit 50";
 		
@@ -195,6 +210,9 @@ class Queue extends Base
 		}
 		if ($owner_id) {
 			$st->bindValue(':owner_id', $owner_id, PDO::PARAM_INT);
+		}
+		if ($parent_id) {
+			$st->bindValue(':parent_id', $parent_id, PDO::PARAM_INT);
 		}
 		
 		if (!$st->execute()) {
