ecda94cc006268e3f09841734fff4ac111e0ab85
Valentin Balt <valentin.balt@gmail.com>
2015-01-18 18:31:57 +0200

    New passwords mechanism

--- core/utl/password.php
+++ core/utl/password.php
@@ -12,7 +12,7 @@ class Password
 		return self::numeric_password();
 	}
 	
-	static private function trivial_password($length=8)
+	static public function trivial_password($length=8)
 	{
 		$base = 'abcdefghijklmnopqrstuvwxyz';
 		$baseD = '0123456789';
@@ -30,8 +30,15 @@ class Password
 		return implode('', $r);
 	}
 	
-	static private function numeric_password($length=8)
+	static public function numeric_password($length=8)
 	{
 		return rand(11111111,99999999);
+	}
+
+	static public function secure_password($length=8)
+	{
+		$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+		$password = substr( str_shuffle( $chars ), 0, $length );
+		return $password;
 	}
 }
