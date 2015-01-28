0f7a70277ad92628d9207e969123e52ab94c65ce
Michael Stepanov <michael@stepanoff.org>
2015-01-28 10:55:22 +0200

    Added new parameters to the get and post methods

--- core/utl/curl.php
+++ core/utl/curl.php
@@ -2,7 +2,7 @@
 
 class Curl
 {
-	static public function post($url, $params, $agent='routePHP App Server')
+	static public function post($url, $params, $agent='routePHP App Server', $auth=array(), $headers = array())
 	{
 		$ch = curl_init();
 		
@@ -11,19 +11,40 @@ class Curl
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_POST, true);
 		curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+		
+		//curl_setopt($ch, CURLOPT_VERBOSE, true);
+		
+		if (!empty($params))
+		{
+			curl_setopt($ch, CURLOPT_POSTFIELDS, is_array($params) ? http_build_query($params) : $params);
+		}
+
+		if (!empty($auth['username']) && !empty($auth['password']))
+		{
+			curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+			curl_setopt($ch, CURLOPT_USERPWD, $auth['username'] . ':' . $auth['password']);
+		}
+
+		if (!empty($headers))
+		{
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		}
 		
 		return curl_exec($ch);
 	}
 	
-	static public function get($url)
+	static public function get($url, $params=array(), $agent='routePHP App Server', $auth=array(), $headers = array())
 	{
 		$ch = curl_init();
 		
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-		
+		//curl_setopt($ch, CURLOPT_VERBOSE, true);
+		if (!empty($headers))
+		{
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		}
 		return curl_exec($ch);
 	}
 }
